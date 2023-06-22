737790868f865516ec62962432af54fe5e28a25b
Ikey Doherty <ikey@serpentos.com>
2023-06-22 16:45:17 +0100

    mkbind: Adapt to make systemd binding buildable
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- mkbind.sh
+++ mkbind.sh
@@ -2,39 +2,41 @@
 set -e
 set -x
 
-dstep -o source/systemd/bus.d \
-        /usr/include/systemd/sd-bus.h \
-        --normalize-modules=true \
-        --public-submodules=true \
-        --rename-enum-members=false \
-        --translate-macros=false \
-        --alias-enum-members=true \
-        --package systemd \
-        --comments=false \
-        --global-attribute '@nogc' \
-        --global-attribute 'nothrow'
-        
-dstep -o source/systemd/journal.d \
-        /usr/include/systemd/sd-journal.h \
-        --normalize-modules=true \
-        --public-submodules=true \
-        --rename-enum-members=false \
-        --translate-macros=false \
-        --alias-enum-members=true \
-        --package systemd \
-        --comments=false \
-        --global-attribute '@nogc' \
-        --global-attribute 'nothrow'
-        
-dstep -o source/systemd/daemon.d \
-        /usr/include/systemd/sd-daemon.h \
-        --normalize-modules=true \
-        --public-submodules=true \
-        --rename-enum-members=false \
-        --translate-macros=false \
-        --alias-enum-members=true \
-        --package systemd \
-        --comments=false \
-        --global-attribute '@nogc' \
-        --global-attribute 'nothrow' \
-        -U__INCLUDE_LEVEL__
+for i in "id128" "bus" "bus-vtable" "journal" "daemon" "event" ; do
+        imports="--global-import core.stdc.stdarg"
+        if [[ $i != "id128" ]]; then
+                imports=" $imports --global-import systemd.id128"
+        fi
+
+        # Fix imports
+        if [[ $i == "bus" ]]; then
+                imports=" $imports --global-import systemd.event --global-import systemd.bus_vtable"
+        elif [[ $i == "bus-vtable" ]]; then
+                imports=" $imports --global-import systemd.bus"
+        elif [[ $i == "event" ]]; then
+                imports=" $imports --global-import core.sys.linux.sys.signalfd --global-import core.sys.linux.sys.inotify"
+        fi
+        imports="--global-import systemd.common $imports"
+        pkgName=`echo $i | sed 's@-@_@g'`
+
+        dstep   /usr/include/systemd/sd-$i.h -o source/systemd/$pkgName.d \
+                --normalize-modules=true \
+                --public-submodules=true \
+                --rename-enum-members=false \
+                --translate-macros=false \
+                --alias-enum-members=true \
+                --package systemd \
+                --comments=false \
+                --global-attribute '@nogc' \
+                --global-attribute 'nothrow' \
+                --skip _sd_useless_struct_to_allow_trailing_semicolon_ \
+                $imports \
+                -D__COVERITY__=1
+
+
+done
+
+
+for i in source/systemd/*.d ; do
+        sed -e 's/ULL/UL/g' -i $i;
+done
