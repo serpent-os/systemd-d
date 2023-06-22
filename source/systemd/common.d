9989f64b39175162ded701a207271b6ff2bd6c8d
Ikey Doherty <ikey@serpentos.com>
2023-06-22 16:45:50 +0100

    Add common.d helper
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/systemd/common.d
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * systemd.common
+ *
+ * Symbols missed by dstep
+ *
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
+ * License: Zlib
+ */
+module systemd.common;
+
+enum SD_ID128_STRING_MAX = 33U;
+enum SD_ID128_UUID_STRING_MAX = 37U;
